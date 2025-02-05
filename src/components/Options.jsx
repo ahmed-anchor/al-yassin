42e1a81639c1d6854581b5b652208d237a9b0fd8
ahmed-anchor <ahmedashrafodi.4@gmail.com>
2025-02-05 23:06:08 +0200

    another commit bro

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -4,28 +4,28 @@ import Link from 'next/link'
 export const Options = () => {
   return (
     <div className='w-full h-[200px]'>
-      <h1 className='text-[26px] sm:text-[38px] text-center py-5'>ليه تختارنا</h1>
-      <ul className='flex flex-col sm:flex-row Zain gap-3 justify-around items-center px-2' >
+      <h1 className='text-[26px] sm:text-[38px] font-bold text-center py-5 Lalezar'>ليه تختارنا</h1>
+      <ul className='flex flex-col sm:flex-row gap-3 justify-around items-center px-2 Zain' >
         <li className='bg-white w-full py-2 rounded-md px-4 sm:w-[200px] lg:w-[280px] text-right h-[150px]' >
           <Link href='#'>
-              <h1 className="text-[21px] pb-2" >المصداقية</h1>
-              <p>
-                نحن نحرص علي علي مصداقيه المكان و الاهتام بخصوصية العميل
-              </p>
+            <h1 className="text-[21px] pb-2 font-bold" >المصداقية</h1>
+            <p className='font-normal' >
+              نحن نحرص علي علي مصداقيه المكان و الاهتام بخصوصية العميل
+            </p>
           </Link>
         </li>
         <li className='bg-white w-full py-2 rounded-md px-4 sm:w-[200px] lg:w-[280px] text-right h-[150px]' >
           <Link href='#'>
-            <h1 className="text-[21px] pb-2" >جودة المنتجات</h1>
-            <p>
+            <h1 className="text-[21px] pb-2 font-bold" >جودة المنتجات</h1>
+            <p className='font-normal' >
               نحرص دائما على جودة منتجاتنا و رضا العلاء بها
             </p>
           </Link>
         </li>
         <li className='bg-white w-full py-2 rounded-md px-4 sm:w-[200px] lg:w-[280px] text-right h-[150px]' >
           <Link href='#'>
-            <h1 className="text-[21px] pb-2" >نهتم لسماع آراكم</h1>
-            <p>
+            <h1 className="text-[21px] pb-2 font-bold" >نهتم لسماع آراكم</h1>
+            <p className='font-normal' >
               نهتم برآراكم لتطوير خدمه مرضيه جدا
             </p>
           </Link>
