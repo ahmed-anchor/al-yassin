fa8a64c827188d635f59b50ee05a7651b00470d0
ahmed-anchor <ahmedashrafodi.4@gmail.com>
2025-02-06 01:27:51 +0200

    another commit

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 
 export const Options = () => {
   return (
     <div className='w-full h-[200px]'>
-      <h1 className='text-[26px] sm:text-[38px] font-bold text-center py-5 Lalezar'>ليه تختارنا</h1>
+      <motion.h1
+        initial={{ opacity: 0, scale: .98 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1, ease: 'easeOut' }}
+        viewport={{ once: true }}
+       className='text-[26px] sm:text-[40px] font-bold text-center py-5 Lalezar text-[#e4eef3]'>ليه تختارنا</motion.h1>
       <ul className='flex flex-col sm:flex-row gap-3 justify-around items-center px-2 Zain' >
-        <li className='bg-white w-full py-2 rounded-md px-4 sm:w-[200px] lg:w-[280px] text-right h-[150px]' >
+        <li className='bg-[#e4eef3] w-full py-2 rounded-md px-4 sm:w-[200px] lg:w-[280px] text-right h-[150px]' >
           <Link href='#'>
             <h1 className="text-[21px] pb-2 font-bold" >المصداقية</h1>
             <p className='font-normal' >
@@ -14,7 +20,7 @@ export const Options = () => {
             </p>
           </Link>
         </li>
-        <li className='bg-white w-full py-2 rounded-md px-4 sm:w-[200px] lg:w-[280px] text-right h-[150px]' >
+        <li className='bg-[#e4eef3] w-full py-2 rounded-md px-4 sm:w-[200px] lg:w-[280px] text-right h-[150px]' >
           <Link href='#'>
             <h1 className="text-[21px] pb-2 font-bold" >جودة المنتجات</h1>
             <p className='font-normal' >
@@ -22,7 +28,7 @@ export const Options = () => {
             </p>
           </Link>
         </li>
-        <li className='bg-white w-full py-2 rounded-md px-4 sm:w-[200px] lg:w-[280px] text-right h-[150px]' >
+        <li className='bg-[#e4eef3] w-full py-2 rounded-md px-4 sm:w-[200px] lg:w-[280px] text-right h-[150px]' >
           <Link href='#'>
             <h1 className="text-[21px] pb-2 font-bold" >نهتم لسماع آراكم</h1>
             <p className='font-normal' >
