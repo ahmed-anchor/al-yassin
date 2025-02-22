523ddb98baef24492df0929154ae3ea0ed1d8340
ahmed-anchor <ahmedashrafodi.4@gmail.com>
2025-02-22 20:06:15 +0200

    another commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 "use client"
-import { Intro } from "@/components/Intro";
 import { SinglePage } from "@/components/SinglePage";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import Lenis from "lenis";
 
 export default function Home() {
-  const [isMounted, setIsMounted] = useState(true);
 
   useEffect(()=> {
     const lenis = new Lenis();
@@ -16,12 +14,5 @@ export default function Home() {
     requestAnimationFrame(raf);
   }, []);
 
-  return isMounted ? 
-  <Intro toggle={()=> setIsMounted(false)} /> 
-  :
-  (
-    <>
-      <SinglePage />
-    </>
-  )
+  return <SinglePage />
 }
