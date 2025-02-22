2d3d16015152d19ccbe28fee01f203f7bc3c7f03
ahmed-anchor <ahmedashrafodi.4@gmail.com>
2025-02-22 19:12:04 +0200

    another commit here

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
-import faba from '../assets/faba.webp'
+
 
 export default function page() {
   return (
     <div>
       <div>
-        <Image src={faba} width={400} height={400} alt='faba image here' />
         <h1>faba</h1>
         <p>
           ماسوشهخسيبرنيمنرشسيمنربشسمينربشسيب
