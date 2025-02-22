9728dc92dfcc15138484ce0c3ea204a1b9461f07
ahmed-anchor <ahmedashrafodi.4@gmail.com>
2025-02-22 20:10:36 +0200

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,7 @@
 "use client"
 import { SinglePage } from "@/components/SinglePage";
-import { useEffect } from "react";
-import Lenis from "lenis";
 
 export default function Home() {
-
-  useEffect(()=> {
-    const lenis = new Lenis();
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
-  }, []);
 
   return <SinglePage />
 }
